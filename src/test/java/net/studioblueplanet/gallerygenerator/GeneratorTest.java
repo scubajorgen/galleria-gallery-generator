20fee38774db3b64a3606951ddc065050a1b37b5
Jorgen <scubajorgen@gmail.com>
2020-05-02 16:10:39 +0200

    Improved testing

--- src/test/java/net/studioblueplanet/gallerygenerator/GeneratorTest.java
+++ src/test/java/net/studioblueplanet/gallerygenerator/GeneratorTest.java
@@ -13,9 +13,9 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import org.mockito.Mock;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Mockito;
-import static org.mockito.Mockito.*;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.any;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -71,7 +71,92 @@ public class GeneratorTest
     public void tearDown()
     {
     }
+    
+    private void validateNewAlbums(Album album1, Album album2)
+    {
+        Image image;
+        
+        assertEquals("", album1.getName());
+        assertEquals("", album1.getDescription());
+        assertEquals("album1", album1.getDirectory());
+        assertEquals("thumbnail.jpg", album1.getThumbnail());
+        
+        assertEquals(3, album1.getImages().size());
+        image=album1.getImages().get(0);
+        assertEquals("album1_image2.jpg", image.getImage());
+        assertEquals("", image.getTitle());
+        assertEquals("", image.getCaption());
+        assertEquals("2020-05-01 19:00:13", image.getCaptureDateTime());
+        assertEquals(null, image.getVideo());
+        
+        image=album1.getImages().get(1);
+        assertEquals("album1_image3.jpg", image.getImage());
+        assertEquals("2020-05-01 20:00:12", image.getCaptureDateTime());
 
+        image=album1.getImages().get(2);
+        assertEquals("album1_image1.jpg", image.getImage());
+        assertEquals("2020-05-02 08:19:00", image.getCaptureDateTime());        
+
+        assertEquals("", album2.getName());
+        assertEquals("", album2.getDescription());
+        assertEquals("album2", album2.getDirectory());
+        assertEquals("thumbnail.jpg", album2.getThumbnail());
+
+        assertEquals(2, album2.getImages().size());
+        image=album2.getImages().get(0);
+        assertEquals("album2_image2.jpg", image.getImage());
+        assertEquals("", image.getTitle());
+        assertEquals("", image.getCaption());
+        assertEquals("2020-04-29 19:00:13", image.getCaptureDateTime());
+    }
+
+
+    private void validateExistingAlbums(Album album1, int noImages1, Album album2, int noImages2)
+    {
+        Image image;
+        
+        assertEquals("Test Album 1", album1.getName());
+        assertEquals("Description 1", album1.getDescription());
+        assertEquals("album1", album1.getDirectory());
+        assertEquals(noImages1, album1.getImages().size());
+
+        image=album1.getImages().get(0);
+        assertEquals("album1_image3.jpg", image.getImage());
+        assertEquals("Image 2", image.getTitle());
+        assertEquals("Description", image.getCaption());
+        assertEquals("2020-05-01 20:00:12", image.getCaptureDateTime());
+        assertEquals(null, image.getVideo());
+        
+        image=album1.getImages().get(1);
+        assertEquals("album1_image1.jpg", image.getImage());
+        assertEquals("2020-05-02 08:19:00", image.getCaptureDateTime());
+
+        if (noImages1==3)
+        {
+            image=album1.getImages().get(2);
+            assertEquals("album1_image2.jpg", image.getImage());
+            assertEquals("", image.getTitle());
+            assertEquals("", image.getCaption());
+            assertEquals("2020-05-01 19:00:13", image.getCaptureDateTime());
+        }
+        
+        assertEquals("Test Album 2", album2.getName());
+        assertEquals("Description 2", album2.getDescription());
+        assertEquals("album2", album2.getDirectory());
+        assertEquals(noImages2, album2.getImages().size());
+        image=album2.getImages().get(0);
+        assertEquals("album2_image2.jpg", image.getImage());
+        assertEquals("Image 1", image.getTitle()); 
+        
+        if (noImages2==2)
+        {
+            image=album2.getImages().get(1);
+            assertEquals("album2_image1.jpg", image.getImage());
+            assertEquals("", image.getTitle());            
+        }
+    }
+
+    
     /**
      * Test of generate method, of class Generator.
      */
@@ -80,14 +165,12 @@ public class GeneratorTest
     {
         ArgumentCaptor<Object>  galleryCaptor;
         ArgumentCaptor<File>    fileCaptor;
-        Album                   album;
         Gallery                 gallery;
-        Image                   image;
         
         galleryCaptor=ArgumentCaptor.forClass(Object.class);
         fileCaptor=ArgumentCaptor.forClass(File.class);
         
-        System.out.println("generate MERGE, new album, no split");
+        System.out.println("generate: MERGE, new album, no split");
         Options options         = new Options();
         options.galleryDirectory="src/test/resources/gallery";
         options.albumDirectory  =null;
@@ -104,26 +187,8 @@ public class GeneratorTest
 
         gallery=(Gallery)galleryCaptor.getValue();
         
-        album=gallery.getAlbum("album1");
-        assertEquals(3, album.getImages().size());
-        image=album.getImages().get(0);
-        assertEquals("album1_image2.jpg", image.getImage());
-        assertEquals("", image.getTitle());
-        assertEquals("", image.getCaption());
-        assertEquals("2020-05-01 19:00:13", image.getCaptureDateTime());
-        assertEquals(null, image.getVideo());
-        
-        image=album.getImages().get(1);
-        assertEquals("album1_image3.jpg", image.getImage());
-        assertEquals("2020-05-01 20:00:12", image.getCaptureDateTime());
+        validateNewAlbums(gallery.getAlbum("album1"), gallery.getAlbum("album2"));
 
-        image=album.getImages().get(2);
-        assertEquals("album1_image1.jpg", image.getImage());
-        assertEquals("2020-05-02 08:19:00", image.getCaptureDateTime());
-
-        album=gallery.getAlbum("album2");
-        assertEquals(2, album.getImages().size());
-        
         verify(backupper, times(0)).backupFile(any(), any());
     }
     
@@ -143,7 +208,7 @@ public class GeneratorTest
         galleryCaptor=ArgumentCaptor.forClass(Object.class);
         fileCaptor=ArgumentCaptor.forClass(File.class);
         
-        System.out.println("generate, MERGE, new album, split");
+        System.out.println("generate: MERGE, new album, split");
         Options options         = new Options();
         options.galleryDirectory="src/test/resources/gallery";
         options.albumDirectory  =null;
@@ -166,6 +231,12 @@ public class GeneratorTest
         album2=(Album)galleryCaptor.getAllValues().get(1);
         gallery=(Gallery)galleryCaptor.getAllValues().get(2);
         
+        assertEquals(null, gallery.getAlbum("album1").getImages());
+        assertNotEquals(null, album1.getImages());
+        assertNotEquals(null, album2.getImages());
+        
+        validateNewAlbums(album1, album2);
+       
         verify(backupper, times(0)).backupFile(any(), any());
     }
 
@@ -179,7 +250,8 @@ public class GeneratorTest
         ArgumentCaptor<Object>  galleryCaptor;
         ArgumentCaptor<File>    fileCaptor;
         ArgumentCaptor<File>    backupFileCaptor;
-        Album                   album;
+        Album                   album1;
+        Album                   album2;
         Gallery                 gallery;
         Image                   image;
         
@@ -205,35 +277,11 @@ public class GeneratorTest
 
         gallery=(Gallery)galleryCaptor.getValue();
         
-        album=gallery.getAlbum("album1");
-        assertEquals("Test Album 1", album.getName());
-        assertEquals("Description 1", album.getDescription());
-        assertEquals("album1", album.getDirectory());
-        assertEquals(3, album.getImages().size());
-        image=album.getImages().get(0);
-        assertEquals("album1_image3.jpg", image.getImage());
-        assertEquals("Image 2", image.getTitle());
-        assertEquals("Description", image.getCaption());
-        assertEquals("2020-05-01 20:00:12", image.getCaptureDateTime());
-        assertEquals(null, image.getVideo());
+        album1=gallery.getAlbum("album1");
+        album2=gallery.getAlbum("album2");
+        validateExistingAlbums(album1, 3, album2, 1);
         
-        image=album.getImages().get(1);
-        assertEquals("album1_image1.jpg", image.getImage());
-        assertEquals("2020-05-02 08:19:00", image.getCaptureDateTime());
-
-        image=album.getImages().get(2);
-        assertEquals("album1_image2.jpg", image.getImage());
-        assertEquals("2020-05-01 19:00:13", image.getCaptureDateTime());
-
-        album=gallery.getAlbum("album2");
-        assertEquals("Test Album 2", album.getName());
-        assertEquals("Description 2", album.getDescription());
-        assertEquals("album2", album.getDirectory());
-        assertEquals(1, album.getImages().size());
-        image=album.getImages().get(0);
-        assertEquals("album2_image2.jpg", image.getImage());
-        assertEquals("Image 1", image.getTitle());
-        assertEquals(null, image.getCaptureDateTime());
+        assertEquals(null, album2.getImages().get(0).getCaptureDateTime());
         
         verify(backupper, times(1)).backupFile(backupFileCaptor.capture(), any());
         assertEquals("src/test/resources/gallery2/gallery.json", backupFileCaptor.getValue().getPath().replace("\\", "/"));
@@ -248,7 +296,8 @@ public class GeneratorTest
         ArgumentCaptor<Object>  galleryCaptor;
         ArgumentCaptor<File>    fileCaptor;
         ArgumentCaptor<File>    backupFileCaptor;
-        Album                   album;
+        Album                   album1;
+        Album                   album2;
         Gallery                 gallery;
         Image                   image;
         
@@ -274,36 +323,113 @@ public class GeneratorTest
 
         gallery=(Gallery)galleryCaptor.getValue();
         
-        album=gallery.getAlbum("album1");
-        assertEquals("Test Album 1", album.getName());
-        assertEquals("Description 1", album.getDescription());
-        assertEquals("album1", album.getDirectory());
-        assertEquals(2, album.getImages().size());
-        image=album.getImages().get(0);
-        assertEquals("album1_image3.jpg", image.getImage());
-        assertEquals("Image 2", image.getTitle());
-        assertEquals("Description", image.getCaption());
-        assertEquals("2020-05-01 20:00:12", image.getCaptureDateTime());
-        assertEquals(null, image.getVideo());
+        album1=gallery.getAlbum("album1");
+        album2=gallery.getAlbum("album2");
+        this.validateExistingAlbums(album1, 2, album2, 1);
         
-        image=album.getImages().get(1);
-        assertEquals("album1_image1.jpg", image.getImage());
-        assertEquals("2020-05-02 08:19:00", image.getCaptureDateTime());
-
-        album=gallery.getAlbum("album2");
-        assertEquals("Test Album 2", album.getName());
-        assertEquals("Description 2", album.getDescription());
-        assertEquals("album2", album.getDirectory());
-        assertEquals(1, album.getImages().size());
-        image=album.getImages().get(0);
-        assertEquals("album2_image2.jpg", image.getImage());
-        assertEquals("Image 1", image.getTitle());
-        assertEquals("2020-04-29 19:00:13", image.getCaptureDateTime());
+        assertEquals("2020-04-29 19:00:13", album2.getImages().get(0).getCaptureDateTime());
         
         verify(backupper, times(1)).backupFile(backupFileCaptor.capture(), any());
         assertEquals("src/test/resources/gallery2/gallery.json", backupFileCaptor.getValue().getPath().replace("\\", "/"));
     }
 
+    /**
+     * Test of generate method, of class Generator.
+     */
+    @Test
+    public void testGenerateSkipExistingGalleryNonSplit() throws IOException
+    {
+        ArgumentCaptor<Object>  galleryCaptor;
+        ArgumentCaptor<File>    fileCaptor;
+        ArgumentCaptor<File>    backupFileCaptor;
+        Album                   album1;
+        Album                   album2;
+        Gallery                 gallery;
+        Image                   image;
+        
+        galleryCaptor   =ArgumentCaptor.forClass(Object.class);
+        fileCaptor      =ArgumentCaptor.forClass(File.class);
+        backupFileCaptor=ArgumentCaptor.forClass(File.class);
+        
+        System.out.println("generate COMPLEMENT, existing gallery, all albums");
+        Options options         = new Options();
+        options.galleryDirectory="src/test/resources/gallery2";
+        options.albumDirectory  =null;
+        options.mode            =Generator.ModeType.MODE_SKIPEXISTING;
+        options.addExifDateTime =true;
+        options.split           =false;
+        Generator instance      = new Generator(mapper, writer, backupper);
+        instance.generate(options);
+        
+        verify(writer, times(1)).writeValue(fileCaptor.capture(), galleryCaptor.capture());
+
+        assertEquals("src/test/resources/gallery2/gallery.json", fileCaptor.getValue().getPath().replace("\\", "/"));
+
+        assertEquals(true, galleryCaptor.getValue() instanceof Gallery);
+
+        gallery=(Gallery)galleryCaptor.getValue();
+        
+        album1=gallery.getAlbum("album1");
+        album2=gallery.getAlbum("album2");
+        this.validateExistingAlbums(album1, 2, album2, 1);
+        assertEquals(null, album2.getImages().get(0).getCaptureDateTime());
+        
+        verify(backupper, times(1)).backupFile(backupFileCaptor.capture(), any());
+        assertEquals("src/test/resources/gallery2/gallery.json", backupFileCaptor.getValue().getPath().replace("\\", "/"));
+    }
+
+    /**
+     * Test of generate method, of class Generator.
+     */
+    @Test
+    public void testGenerateExistingGallerySplit() throws IOException
+    {
+        ArgumentCaptor<Object>  galleryCaptor;
+        ArgumentCaptor<File>    fileCaptor;
+        ArgumentCaptor<File>    backupFileCaptor;
+        Album                   album1;
+        Album                   album2;
+        Gallery                 gallery;
+        Image                   image;
+        
+        galleryCaptor   =ArgumentCaptor.forClass(Object.class);
+        fileCaptor      =ArgumentCaptor.forClass(File.class);
+        backupFileCaptor=ArgumentCaptor.forClass(File.class);
+        
+        System.out.println("generate, MERGE, existing gallery, split");
+        Options options         = new Options();
+        options.galleryDirectory="src/test/resources/gallery2";
+        options.albumDirectory  =null;
+        options.mode            =Generator.ModeType.MODE_MERGEEXISTING;
+        options.addExifDateTime =true;
+        options.split           =true;
+        Generator instance      = new Generator(mapper, writer, backupper);
+        instance.generate(options);
+        
+        verify(writer, times(3)).writeValue(fileCaptor.capture(), galleryCaptor.capture());
+
+        assertEquals("src/test/resources/gallery2/album1/album.json", fileCaptor.getAllValues().get(0).getPath().replace("\\", "/"));
+        assertEquals("src/test/resources/gallery2/album2/album.json", fileCaptor.getAllValues().get(1).getPath().replace("\\", "/"));
+        assertEquals("src/test/resources/gallery2/gallery.json", fileCaptor.getAllValues().get(2).getPath().replace("\\", "/"));
+
+        assertEquals(true, galleryCaptor.getAllValues().get(0) instanceof Album);
+        assertEquals(true, galleryCaptor.getAllValues().get(1) instanceof Album);
+        assertEquals(true, galleryCaptor.getAllValues().get(2) instanceof Gallery);
+
+        album1=(Album)galleryCaptor.getAllValues().get(0);
+        album2=(Album)galleryCaptor.getAllValues().get(1);
+        gallery=(Gallery)galleryCaptor.getAllValues().get(2);
+        
+        assertEquals(null, gallery.getAlbum("album1").getImages());
+        assertEquals(null, gallery.getAlbum("album2").getImages());
+        assertNotEquals(null, album1.getImages());
+        assertNotEquals(null, album2.getImages());
+        
+        validateExistingAlbums(album1, 3, album2, 2);
+       
+        verify(backupper, times(1)).backupFile(backupFileCaptor.capture(), any());
+        assertEquals("src/test/resources/gallery2/gallery.json", backupFileCaptor.getValue().getPath().replace("\\", "/"));
+    }
 
     
 }
